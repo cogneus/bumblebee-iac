20df1bf0f6dbcd7f25e5f87ee700c91f9ed1b746
Seb Craft <seb@whereyouat.co.uk>
2024-11-20 08:28:30 +0000

    update promote

--- lib/utils/ssm.ts
+++ lib/utils/ssm.ts
@@ -1,14 +1,25 @@
-import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm"
-import { Config } from "../../scripts/config"
+import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
+import { Config } from "../../scripts/config";
 
-export const getSSMValue = async (name: string, config: Config, defaultValue: string) => {
-  const { region } = config
-  const client = new SSMClient({
-    region,
-  })
-  const result = await client.send(new GetParameterCommand({
-    Name: name,
-    WithDecryption: false,
-  }))
-  return result.Parameter?.Value || defaultValue
-}
+export const getSSMValue = async (
+  name: string,
+  config: Config,
+  defaultValue: string
+) => {
+  let result;
+  try {
+    const { region } = config;
+    const client = new SSMClient({
+      region,
+    });
+    result = await client.send(
+      new GetParameterCommand({
+        Name: name,
+        WithDecryption: false,
+      })
+    );
+  } finally {
+  }
+
+  return result?.Parameter?.Value ?? defaultValue;
+};
